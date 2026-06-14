2e3b302bf817bf3c2b8e02581d0574f6a00bd1ec
Rob <rob@websanova.com>
2026-06-14 15:00:51 -0400

    Fix for css to load and add polling to detect changes (windows).

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -15,8 +15,11 @@ export default defineConfig(({ mode }) => {
             }
         },
         server: {
-            host: env.VITE_HOST || "localhost",
-            port: Number(env.VITE_PORT) || 5174
+            host: env.VITE_HOST || "0.0.0.0",
+            port: Number(env.VITE_PORT) || 5174,
+            watch: {
+                usePolling: true
+            }
         }
     }
 })
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -15,8 +15,11 @@ export default defineConfig(({ mode }) => {
             }
         },
         server: {
-            host: env.VITE_HOST || "localhost",
-            port: Number(env.VITE_PORT) || 5173
+            host: env.VITE_HOST || "0.0.0.0",
+            port: Number(env.VITE_PORT) || 5173,
+            watch: {
+                usePolling: true
+            }
         }
     }
 })
