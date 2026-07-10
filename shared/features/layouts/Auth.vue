1f9233e355ca7c28d8e7744fdee40de2bbd8fd14
websanova <rob@websanova.com>
2026-07-10 09:13:15 -0400

    Clean up auth layout.

--- shared/features/layouts/Auth.vue
+++ shared/features/layouts/Auth.vue
@@ -9,14 +9,12 @@
       <LogoNav :to="{ name: 'auth-landing' }" />
     </div>
 
-    <div class="flex w-full max-w-sm flex-col items-center gap-4 p-8">
-      <div class="w-full">
-        <RouterView v-slot="{ Component }">
-          <PageTransition>
-            <component :is="Component" />
-          </PageTransition>
-        </RouterView>
-      </div>
+    <div class="flex w-full max-w-sm p-8">
+      <RouterView v-slot="{ Component }">
+        <PageTransition>
+          <component :is="Component" />
+        </PageTransition>
+      </RouterView>
     </div>
   </div>
 </template>
