f9a2d0117189689752f0c35b71d9fa451728971a
websanova <rob@websanova.com>
2026-07-15 17:06:48 -0400

    Clean up sonner components.

--- shared/components/ui/sonner/Sonner.vue
+++ shared/components/ui/sonner/Sonner.vue
@@ -1,14 +1,19 @@
-<script setup lang="ts">
-  import { Toaster as Sonner, type ToasterProps } from "vue-sonner"
+<script lang="ts" setup>
+  import type { ToasterProps } from "vue-sonner"
+  import { CircleCheckIcon, InfoIcon, Loader2Icon, OctagonXIcon, TriangleAlertIcon, XIcon } from "@lucide/vue"
+  import { Toaster as Sonner } from "vue-sonner"
+  import { cn } from '@shared/lib/utils'
   import "vue-sonner/style.css"
 
-  defineProps<ToasterProps>()
+  const props = defineProps<ToasterProps>()
 </script>
 
 <template>
   <Sonner
+    v-bind="props"
     :duration="2000"
     rich-colors
+    :class="cn('toaster group', props.class)"
     :style="{
       '--success-bg': 'var(--success)',
       '--success-text': 'var(--success-foreground)',
@@ -19,6 +24,30 @@
       '--warning-bg': 'var(--warning)',
       '--warning-text': 'var(--warning-foreground)',
       '--warning-border': 'var(--warning)',
+      '--normal-bg': 'var(--primary)',
+      '--normal-text': 'var(--primary-foreground)',
+      '--normal-border': 'var(--primary)',
     }"
-  />
+  >
+    <template #success-icon>
+      <CircleCheckIcon />
+    </template>
+    <template #info-icon>
+      <InfoIcon />
+    </template>
+    <template #warning-icon>
+      <TriangleAlertIcon />
+    </template>
+    <template #error-icon>
+      <OctagonXIcon />
+    </template>
+    <template #loading-icon>
+      <div>
+        <Loader2Icon class="animate-spin" />
+      </div>
+    </template>
+    <template #close-icon>
+      <XIcon />
+    </template>
+  </Sonner>
 </template>
