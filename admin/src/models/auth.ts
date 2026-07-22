4e71a35c4dd1826a8e02d8b47a8478994aea8a30
websanova <rob@websanova.com>
2026-07-22 07:46:53 -0400

    Add locale and timezeon params to admin auth model.

--- admin/src/models/auth.ts
+++ admin/src/models/auth.ts
@@ -4,7 +4,9 @@ export interface AuthDto {
   first_name: string
   id: number
   last_name: string
+  locale: string
   role?: string
+  timezone: string
 }
 
 export interface Auth {
@@ -13,7 +15,9 @@ export interface Auth {
   firstName: string
   id: number
   lastName: string
+  locale: string
   role?: string
+  timezone: string
 }
 
 export function toAuth(dto: AuthDto): Auth {
@@ -23,6 +27,8 @@ export function toAuth(dto: AuthDto): Auth {
     firstName: dto.first_name,
     id: dto.id,
     lastName: dto.last_name,
+    locale: dto.locale,
     role: dto.role,
+    timezone: dto.timezone,
   }
 }
