e1366b1ccd15efae8f77850f912b6e2d73b6fabb
websanova <rob@websanova.com>
2026-07-23 22:14:14 -0400

    Update confirm verify form note and center otp inputs.

--- app/src/features/forms/AuthVerificationConfirm.vue
+++ app/src/features/forms/AuthVerificationConfirm.vue
@@ -2,6 +2,7 @@
   import { computed, ref } from 'vue'
   import { useLogout } from '@shared/composables/support/logout'
   import { useSettingsStore } from '@shared/stores/settings'
+  import { ButtonLoading } from '@shared/components/common/ButtonLoading'
   import { Form, FormButton } from '@shared/components/common/Form'
   import { InputOTP, InputOTPGroup, InputOTPSlot } from '@shared/components/ui/input-otp'
 
@@ -10,28 +11,48 @@
 
   const length = computed(() => settings.data.verificationCodeLength)
   const code = ref('')
+  const isResending = ref(false)
+
+  function onResend() {}
 </script>
 
 <template>
   <Form>
-    <div class="flex flex-col gap-4 self-center">
-      <InputOTP
-        v-model="code"
-        :maxlength="length"
-      >
-        <InputOTPGroup
-            v-for="i in length"
-            :key="i"
+    <div class="flex flex-col gap-4">
+      <p class="text-center">
+        {{ $t('features.form.verification_confirm.note') }}
+      </p>
+
+      <div class="flex justify-center">
+        <InputOTP
+          v-model="code"
+          :maxlength="length"
         >
-          <InputOTPSlot :index="i - 1" />
-        </InputOTPGroup>
-      </InputOTP>
+          <InputOTPGroup
+              v-for="i in length"
+              :key="i"
+          >
+            <InputOTPSlot :index="i - 1" />
+          </InputOTPGroup>
+        </InputOTP>
+      </div>
 
       <FormButton class="w-full">
         {{ $t('features.lbl.verify') }}
       </FormButton>
 
       <p class="self-end text-sm text-muted-foreground">
+        <ButtonLoading
+          :pending="isResending"
+          variant="link"
+          class="h-auto p-0 text-link"
+          @click="onResend"
+        >
+          {{ $t('features.form.verification_confirm.resend_code') }}
+        </ButtonLoading>
+
+        |
+
         <button
           type="button"
           class="text-link"
