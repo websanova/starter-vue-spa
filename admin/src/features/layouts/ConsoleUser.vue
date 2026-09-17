d36cff83181990b2f029cf29862b082aeb0bdedc
websanova <rob@websanova.com>
2026-09-17 11:11:28 -0400

    Hook up nav tabs with users show page.

--- admin/src/features/layouts/ConsoleUser.vue
+++ admin/src/features/layouts/ConsoleUser.vue
@@ -4,6 +4,7 @@
   import { useUser } from '@/composables/api/users'
   import { Heading } from '@shared/components/common/Heading'
   import { Load } from '@shared/components/common/Load'
+  import { NavTab, NavTabs } from '@shared/components/common/NavTabs'
   import PageTransition from '@shared/features/transitions/Page.vue'
 
   const route = useRoute()
@@ -30,6 +31,24 @@
       :error="error"
       :is-pending="isPending"
     >
+      <NavTabs class="mb-3">
+        <NavTab :to="{ name: 'user-users-show-edit' }">
+          {{ $t('features.lbl.edit') }}
+        </NavTab>
+
+        <NavTab :to="{ name: 'user-users-show-info' }">
+          {{ $t('features.lbl.info') }}
+        </NavTab>
+
+        <NavTab :to="{ name: 'user-users-show-billing' }">
+          {{ $t('features.lbl.billing') }}
+        </NavTab>
+
+        <NavTab :to="{ name: 'user-users-show-bookmarks' }">
+          {{ $t('features.lbl.bookmarks') }}
+        </NavTab>
+      </NavTabs>
+
       <RouterView v-slot="{ Component }">
         <PageTransition>
           <component
