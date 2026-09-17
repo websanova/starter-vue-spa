bc3cbfb4da479eef385244dbdfd16937a946ae97
websanova <rob@websanova.com>
2026-09-16 20:19:55 -0400

    Remove user folders.

--- admin/src/router/routes/console.ts
+++ admin/src/router/routes/console.ts
@@ -8,7 +8,6 @@ const Users         = () => import('@/views/console/Users.vue')
 const UserBilling   = () => import('@/views/console/user/Billing.vue')
 const UserBookmarks = () => import('@/views/console/user/Bookmarks.vue')
 const UserEdit      = () => import('@/views/console/user/Edit.vue')
-const UserFolders   = () => import('@/views/console/user/Folders.vue')
 const UserInfo      = () => import('@/views/console/user/Info.vue')
 
 export default [{
@@ -83,10 +82,6 @@ export default [{
       path: 'edit',
       name: 'user-users-show-edit',
       component: UserEdit,
-    }, {
-      path: 'folders',
-      name: 'user-users-show-folders',
-      component: UserFolders,
     }, {
       path: 'info',
       name: 'user-users-show-info',
