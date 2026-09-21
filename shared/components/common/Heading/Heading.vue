f3d306ce60c6cffbed7bd04a91ef4df855a44528
websanova <rob@websanova.com>
2026-09-21 19:38:36 -0400

    Fix for heading to match height of inputs.

--- shared/components/common/Heading/Heading.vue
+++ shared/components/common/Heading/Heading.vue
@@ -12,7 +12,7 @@
 
 <template>
   <h1
-    class="text-2xl mt-3 font-bold"
+    class="text-2xl mt-3 leading-9 font-bold"
     :class="{ 'mb-3': margin, 'pb-2 border-b': divider, 'text-center': center }"
   >
     <slot />
